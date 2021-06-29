78e1ca5ab7f18a3c1aa9bec6083354de6118783f
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-29 09:16:28 -0500

    implemented fail for test stubs

---
+++ src/academy/learnProgramming/BankAccountTest.java
@@ -0,0 +1,23 @@
+package academy.learnProgramming;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() throws Exception{
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void withdrawal() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void getBalance() throws Exception{
+        fail("This test has yet to be implemented");
+    }
+}
+
+
---
+++ src/academy/learnProgramming/Main.java
@@ -0,0 +1,9 @@
+package academy.learnProgramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("The application is running");
+    }
+}
