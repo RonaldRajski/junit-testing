c106836345a71c95f79b682533c2e3c457fdef88
Ronald Rajski <ronaldrajski@gmail.com>
2021-07-19 23:39:10 -0500

    initial commit

--- src/academy/learnProgramming/BankAccountTest.java
+++ src/academy/learnProgramming/BankAccountTest.java
@@ -1,15 +1,27 @@
 package academy.learnProgramming;
 
-import java.awt.*;
-
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+
+  private BankAccount account;
+
+
+
+    @org.junit.Before
+    public void setup(){
+        account = new BankAccount("Ron", "Ski", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+
+    }
+
+
+
     @org.junit.Test
     public void deposit() throws Exception{
 //        fail("This test has yet to be implemented");
-        BankAccount account = new BankAccount("Ron", "Ski", 1000.00);
+//        BankAccount account = new BankAccount("Ron", "Ski", 1000.00, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200.00, balance, 0);
         assertEquals(1200.00, account.getBalance(), 0);
@@ -24,7 +36,7 @@ public class BankAccountTest {
     @org.junit.Test
     public void getBalance_deposit() throws Exception{
 //        fail("This test has yet to be implemented");
-        BankAccount account = new BankAccount("Ron", "Ski", 1000.00);
+//        BankAccount account = new BankAccount("Ron", "Ski", 1000.00, BankAccount.CHECKING);
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
     }
@@ -32,10 +44,18 @@ public class BankAccountTest {
     @org.junit.Test
     public void getBalance_withdraw() throws Exception{
 //        fail("This test has yet to be implemented");
-        BankAccount account = new BankAccount("Ron", "Ski", 1000.00);
+//        BankAccount account = new BankAccount("Ron", "Ski", 1000.00, BankAccount.CHECKING);
         account.withdraw(200.00, true);
         assertEquals(800.00, account.getBalance(), 0);
     }
+
+    @org.junit.Test
+    public void isChecking_true(){
+//        BankAccount account = new BankAccount("Ron", "Ski", 1000.00, BankAccount.CHECKING);
+        assertTrue(account.isChecking());
+    }
+
+
 
 
 //    @org.junit.Test
